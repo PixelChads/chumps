a4483ceb79230dcb7ca951e72e630613caf2314c
PixelChads <PixelChads@protonmail.com>
2025-02-09 20:17:50 +0100

    ...

--- Chub_Jump_test.service.worker.js
+++ Chub_Jump_test.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739127360|10559132272';
+const CACHE_VERSION = '1739128543|11742012321';
 /** @type {string} */
 const CACHE_PREFIX = 'Chub_Jump-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
